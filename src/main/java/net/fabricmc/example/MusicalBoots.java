1470ca9fce52c094897152145d41b5bba7a2531b
FriskySaga <28899815+FriskySaga@users.noreply.github.com>
2022-10-01 22:17:19 -0400

    Deviate from tutorial

--- src/main/java/net/fabricmc/example/MusicalBoots.java
+++ src/main/java/net/fabricmc/example/MusicalBoots.java
@@ -2,14 +2,18 @@ package net.fabricmc.example;
 
 // minecraft imports
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
+import net.minecraft.world.World;
 
 public class MusicalBoots extends ArmorItem {
     public MusicalBoots() {
         super(ModArmorMaterials.MUSICAL, EquipmentSlot.FEET, new FabricItemSettings().group(ModItemGroup.MUSICAL));
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, world, entity, slot, selected);
     }
 }
